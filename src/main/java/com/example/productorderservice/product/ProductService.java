376bef683e06d8684cfbcabefa01aecc8ef38283
temisonehj <himedia.hj@gmail.com>
2023-04-06 15:44:35 +0900

    POJO 상품 수정 기능 구현

--- src/main/java/com/example/productorderservice/product/ProductService.java
+++ src/main/java/com/example/productorderservice/product/ProductService.java
@@ -32,4 +32,11 @@ class ProductService {
         final GetProductResponse response = new GetProductResponse(product.getId(), product.getName(), product.getPrice(), product.getDiscountPolicy());
         return ResponseEntity.ok(response);
     }
+
+    public void updateProdut(Long productId, UpdateProductRequest request) {
+        final Product product = productPort.getProduct(productId);
+        product.update(request.name(), request.price(), request.discountPolicy());
+
+        productPort.save(product);
+    }
 }
--- src/test/java/com/example/productorderservice/product/ProductServiceTest.java
+++ src/test/java/com/example/productorderservice/product/ProductServiceTest.java
@@ -1,28 +1,49 @@
 package com.example.productorderservice.product;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-@SpringBootTest
 class ProductServiceTest {
 
-    @Autowired
     private ProductService productService;
+    private StubProductPort productPort = new StubProductPort();
 
-    @Test
-    void 상품조회() {
-        // 상품등록
-        productService.addProduct(ProductSteps.상품등록요청_생성());
-        final long productId = 1L;
-
-        // 상품 조회
-        //final GetProductResponse response = productService.getProduct(productId);
-
-        // 상품의 응답 검증
-        //assertThat(response).isNotNull();
+    @BeforeEach
+    void setUp() {
+        productService = new ProductService(productPort);
     }
 
+    @Test
+    void 상품수정() {
+
+        final Long productId = 1L;
+
+        final UpdateProductRequest request = new UpdateProductRequest("상품 수정", 2000, DiscountPolicy.NONE);
+
+        final Product product = new Product("상품명", 1000, DiscountPolicy.NONE);
+
+        productPort.getProduct_will_return = product;
+
+        productService.updateProdut(productId, request);
+
+        assertThat(product.getName()).isEqualTo("상품 수정");
+        assertThat(product.getPrice()).isEqualTo(2000);
+    }
+
+    private static class StubProductPort implements ProductPort {
+        public Product getProduct_will_return;
+
+        @Override
+        public void save(Product product) {
+
+        }
+
+        @Override
+        public Product getProduct(long productId) {
+
+            return getProduct_will_return;
+        }
+    }
 }
