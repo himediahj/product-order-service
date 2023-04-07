33965d713a2254fcb51197c4c18edf1d682b7624
temisonehj <himedia.hj@gmail.com>
2023-04-07 14:51:09 +0900

    상품 주문 springboot test

--- src/test/java/com/example/productorderservice/order/OrderServiceTest.java
+++ src/test/java/com/example/productorderservice/order/OrderServiceTest.java
@@ -2,21 +2,25 @@ package com.example.productorderservice.order;
 
 import com.example.productorderservice.product.DiscountPolicy;
 import com.example.productorderservice.product.Product;
-import com.example.productorderservice.product.ProductRepository;
+import com.example.productorderservice.product.ProductService;
+import com.example.productorderservice.product.ProductSteps;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
-import org.springframework.util.Assert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.HashMap;
-import java.util.Map;
-
+@SpringBootTest
 class OrderServiceTest {
 
+    @Autowired
     private OrderService orderService;
+
+    @Autowired
+    private ProductService productService;
     private OrderPort orderPort;
     private OrderRepository orderRepository;
 
-    @BeforeEach
+    /*@BeforeEach
     void setUp() {
         orderRepository = new OrderRepository();
         orderPort = new OrderPort() {
@@ -31,99 +35,19 @@ class OrderServiceTest {
             }
         };
         orderService = new OrderService(orderPort);
-    }
+    }*/
 
     @Test
     void 상품주문() {
+        productService.addProduct(ProductSteps.상품등록요청_생성());
+        final CreateOrderRequest request = 상품주문요청_생성();
+        orderService.createOrder(request);
+    }
+
+    private static CreateOrderRequest 상품주문요청_생성() {
         final Long productId = 1L;
         final int quantity = 2;
-        final CreateOrderRequest request = new CreateOrderRequest(productId, quantity);
-        orderService.createOrder(request);
-
-
+        return new CreateOrderRequest(productId, quantity);
     }
 
-    private record CreateOrderRequest(Long productId, int quantity) {
-        private CreateOrderRequest {
-            Assert.notNull(productId, "상품 ID는 필수입니다.");
-            Assert.isTrue(quantity > 0, "수량은 0보다 커야 합니다.");
-        }
-    }
-
-    private class OrderService {
-        private final OrderPort orderPort;
-
-        private OrderService(final OrderPort orderPort) {
-            this.orderPort = orderPort;
-        }
-
-        public void createOrder(final CreateOrderRequest request) {
-            final Product product = orderPort.getProductById(request.productId);
-
-            final Order order = new Order(product, request.quantity());
-
-            orderPort.save(order);
-        }
-
-
-
-    }
-
-    private class Order {
-        private final Product product;
-        private final int quantity;
-        private Long id;
-
-        public Order(final Product product, final int quantity) {
-            this.product = product;
-            this.quantity = quantity;
-            Assert.notNull(product, "상품은 필수입니다.");
-            Assert.isTrue(quantity > 0, "수량은 0보다 커야 합니다.");
-
-        }
-
-        public Long getId() {
-            return id;
-        }
-
-        public void assignId(final Long id) {
-            this.id = id;
-        }
-    }
-
-    private class OrderAdapter implements OrderPort {
-        private final ProductRepository productRepository;
-        private final OrderRepository orderRepository;
-
-        private OrderAdapter(final ProductRepository productRepository, OrderRepository orderRepository) {
-            this.productRepository = productRepository;
-            this.orderRepository = orderRepository;
-        }
-
-        @Override
-        public Product getProductById(final Long productId) {
-            return productRepository.findById(productId).orElseThrow(() -> new IllegalArgumentException("상품이 존재하지 않습니다."));
-        }
-
-        @Override
-        public void save(final Order order) {
-            orderRepository.save(order);
-        }
-
-
-    }
-    private class OrderRepository {
-        private Map<Long, Order> persistence = new HashMap<>();
-        private Long sequence = 0L;
-        public void save(Order order) {
-            order.assignId(++sequence);
-            persistence.put(order.getId(), order);
-        }
-    }
-
-    private interface OrderPort {
-        Product getProductById(final Long productId);
-
-        void save(final Order order);
-    }
 }
